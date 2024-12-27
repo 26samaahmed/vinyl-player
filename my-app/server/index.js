d5848b3b1708ccbde9d9b3a00f94ef86260cf356
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-12-26 17:05:59 -0800

    start creating express app

---
+++ my-app/server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const port = 5000
+
+dotenv.config(); // Load environment variables from .env file
+
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+
+var app = express();
+app.get('/auth/login', (req, res) => {
+});
+
+app.get('/auth/callback', (req, res) => {
+});
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`) } );
